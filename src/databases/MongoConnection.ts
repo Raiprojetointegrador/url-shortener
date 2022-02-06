131fe88d8da6f6f1bc07690690b3ec455f201ada
Thales Macêdo <thalesmacedo1@live.com>
2022-02-05 23:12:32 -0300

    created mongodb class and connection

---
+++ src/databases/MongoConnection.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+export class MongoConnection {
+    public async connect(): Promise<void> {
+        try{
+            await mongoose.connect(process.env.MONGO_URL);
+            console.log('Connected to MongoDB');
+        } catch (err) {
+            console.log(err.message);
+            process.exit(1);
+        }
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
+import { MongoConnection } from './databases/MongoConnection';
 import express from 'express';
 import { Url } from './controllers/Url';
 
 const api = express();
 api.use(express.json());
+
+const database = new MongoConnection;
+database.connect();
 
 const url = new Url();
 api.post('/shorten', url.shorten);
